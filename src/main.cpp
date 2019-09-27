9c74f3c5e82073c4a321f895c2dbaa47bf09ad56
Liverson Paulo <liverson.p@gmail.com>
2019-09-26 23:12:42 -0300

    Developing modo_venda that sales products to the client and uses processo_venda to do that
    Implemented processo_venda where do the process of saling products in modo_venda

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <vector>
 #include "funcionario.hpp"
 #include "produto.hpp"
 #include "cliente.hpp"
+#include "associado.hpp"
+#include "pagamento.hpp"
 
 #ifdef __WIN32
 	#define CLEAR  "cls"
@@ -15,14 +18,15 @@
 using namespace std;
 
 //Catálogo de funções que são utilizadas durante o programa.
+string get_string();
+template <typename T1>
+T1 getInput();
 void menu_funcionario(vector <Funcionario*>& funcionarios_cadastrados);
 void loja(vector <Produto*>& produtos_da_padaria, vector <Cliente*>& clientes_da_padaria);
 void modo_venda(vector <Produto*>& produtos_da_padaria, vector <Cliente*>& clientes_da_padaria);
+void processo_venda(vector <Produto*>& produtos_da_padaria, Cliente *cliente_atual);
 void modo_estoque(vector <Produto*>& produtos_da_padaria);
-string get_string();
-
-template <typename T1>
-T1 getInput();
+void modo_recomendacao(vector <Produto*>& produtos_da_padaria, vector <Cliente*>& clientes_da_padaria);
 
 int main(){
 	vector <Cliente*> clientes;
@@ -141,7 +145,7 @@ void loja(vector <Produto*>& produtos_da_padaria, vector <Cliente*>& clientes_da
 		cout << "3 - Modo recomendação" << endl;
 		cout << "0 - Sair da loja" << endl;
 		cout << "->> ";
-		cin >> escolha;
+		escolha = getInput<int>();
 
 		if (escolha >=0 && escolha <=3){
 			switch(escolha){
@@ -160,7 +164,7 @@ void loja(vector <Produto*>& produtos_da_padaria, vector <Cliente*>& clientes_da
 				case 3:
 					system(CLEAR);
 					cout << "Modo recomendação" << endl;
-				//	modo_recomendacao();
+					modo_recomendacao(produtos_da_padaria,clientes_da_padaria);
 					cout << "\nDigite qualquer tecla para poder continuar..." << endl;
 					cin >> pause;
 					break;
@@ -175,7 +179,106 @@ void loja(vector <Produto*>& produtos_da_padaria, vector <Cliente*>& clientes_da
 }
 
 void modo_venda(vector <Produto*>& produtos_da_padaria, vector <Cliente*>& clientes_da_padaria){
-	cout << "ENOIS" << endl;
+	string nome_do_cliente,cpf_do_cliente;
+	int idade_do_cliente;
+	bool eh_cliente=false;
+	Cliente *cliente_atual;
+	Pagamento *a_pagar;
+	cout << "---------------INFORMAÇÕES DO CLIENTE---------------" << endl;
+	cout << "Digite o nome do cliente: ";
+	nome_do_cliente = getString();
+
+	for(auto buscador : clientes_da_padaria){
+		cout << buscador-> get_nome() << endl;
+		if(nome_do_cliente == buscador-> get_nome()){
+			cout << "Cliente já cadastrado encontrado com sucesso!" << endl;
+			eh_cliente = true;
+		}
+	}
+
+	if(eh_cliente){
+		for(auto buscador :clientes_associados){
+
+		}
+		processo_venda(produtos_da_padaria, cliente_atual);
+	}
+
+	else{
+		cout << "Digite a idade do cliente: ";
+		idade_do_cliente = getInput<int>();
+
+		cout << "Digite o CPF do cliente: ";
+		cpf_do_cliente = getString();
+		cliente_atual = new Cliente();
+
+		cliente_atual->set_nome(nome_do_cliente);
+		cliente_atual->set_idade(idade_do_cliente);
+		cliente_atual->set_cpf(cpf_do_cliente);
+		processo_venda(produtos_da_padaria,cliente_atual);
+		clientes_da_padaria.push_back(cliente_atual);
+	}
+}
+
+void processo_venda(vector <Produto*>& produtos_da_padaria, Cliente *cliente_atual){
+	string produto_do_cliente, forma_de_pagamento;
+	int quantidade,valor_final_calculo=0,valor_item;
+	bool tem_produto;
+	float valor_final=0;
+
+	system(CLEAR);
+
+	cout << "INSTRUÇÕES:" << endl;
+	cout << "Aqui serão adicionados os produtos no carrinho do cliente" << endl;
+	cout << "Para finalizar a lista, basta digitar fim" << endl << endl;
+	while(1){
+		tem_produto = false;
+		cout << "Digite o produto desejado: ";
+		produto_do_cliente = getString();
+		if(produto_do_cliente == "fim")
+			break;
+
+		for(Produto* buscador : produtos_da_padaria){
+			if(buscador->get_nome() == produto_do_cliente){
+				tem_produto = true;
+				cout << "Digite a quantidade do produto em questão: ";
+				quantidade = getInput<int>();
+				if(buscador->get_quantidade() >= quantidade){
+					cliente_atual->set_produtos_carrinho(produto_do_cliente,quantidade,buscador->get_preco());
+					cliente_atual-> set_produtos_comprados(produto_do_cliente);
+					valor_item = (buscador->get_preco()*100*quantidade);
+					valor_final_calculo += valor_item;	
+				}
+				else if(buscador->get_quantidade() < quantidade){
+					cout << "Erro!" << endl;
+					cout << "Não há quantidade o suficiente no estoque!!" << endl;
+				}
+			}
+		}
+		if(tem_produto == false){
+			cout << "Erro!" << endl;
+			cout << "Não há este produto no estoque!!" << endl;
+		}
+	}
+
+	system(CLEAR);
+	if(!cliente_atual->get_produtos_carrinho().empty()){
+		cout << "---------------PRODUTOS NO SEU CARRINHO---------------" << endl;
+		auto produtos_do_cliente = cliente_atual->get_produtos_carrinho();
+		for(auto buscador : produtos_do_cliente){
+			cout << "Produto: " <<get<0> (buscador) << endl; 
+			cout << "Quantidade: " << get<1>(buscador) << endl;
+			cout << "valor: R$" << fixed << setprecision(2) << get<2> (buscador) << endl;
+		}
+		valor_final = cliente_atual->calculo_pagamento(valor_final_calculo);
+		cliente_atual->set_valor_de_compras(valor_final);
+		cout << "Valor a pagar: R$" << fixed << setprecision(2) << cliente_atual->get_valor_de_compras() << endl;
+		cout << "Digite a forma de pagamento: ";
+		cin >> forma_de_pagamento;
+
+	}
+	else{
+		cout << "Não há itens!!" << endl;
+	}
 }
 
 void modo_estoque(vector <Produto*>& produtos_da_padaria){
@@ -187,7 +290,7 @@ void modo_estoque(vector <Produto*>& produtos_da_padaria){
 	Produto *produto_da_padaria;
 	while(continua){
 		system(CLEAR);
-		cout << "---------------MODO ESTOQUE---------------\n";
+		cout << "---------------MODO ESTOQUE---------------" << endl;
 		cout << "Digite a opção desejada:" << endl;
 		cout << "1 - Listar produtos que há na loja." << endl;
 		cout << "2 - Adicionar produtos." << endl;
@@ -200,21 +303,23 @@ void modo_estoque(vector <Produto*>& produtos_da_padaria){
 				case 1:
 					system(CLEAR);
 					if(!produtos_da_padaria.empty()){
+						cout << "---------------PRODUTOS NO ESTOQUE---------------" << endl;
 						for(Produto *buscador : produtos_da_padaria){
-							cout << "-------------------------------------\n";
+							cout << endl << "---------------------------------------------------------" << endl;
 							cout << "Nome: ";
 							cout << buscador -> get_nome() << endl;
 							cout << "Quantidade: ";
 							cout << buscador ->get_quantidade() << endl;
-							cout << "Preço: ";
-							cout << buscador ->get_preco() << endl;
+							cout << fixed;
+							cout << "Preço: R$";
+							cout << setprecision(2) <<buscador ->get_preco() << endl;
 							cout << "Categorias:" << endl;
 							auto categ = buscador->get_categorias();
 							for(string categories : categ){
-								cout << categories << endl;
+									cout << categories << " ";
 							}
 						}
-						cout << "-------------------------------------\n";
+						cout<< endl << "---------------------------------------------------------" << endl;
 					}
 					else{
 						cout << "Não há produtos!!!" << endl;
@@ -280,4 +385,8 @@ void modo_estoque(vector <Produto*>& produtos_da_padaria){
 			}
 		}
 	}
+}
+
+void modo_recomendacao(vector <Produto*>& produtos_da_padaria, vector <Cliente*>& clientes_da_padaria){
+	cout << "---------------MODO ESTOQUE---------------" << endl;
 }
